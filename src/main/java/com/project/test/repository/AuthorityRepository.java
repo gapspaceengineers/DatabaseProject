d7804c451b99181db860a78c34e2534028c0afdb
gapspace@engineer.com <1Biggithubacount!>
2021-06-12 15:54:15 +0100

    Initial version of test generated by JHipster-7.0.1

---
+++ src/main/java/com/project/test/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.project.test.repository;
+
+import com.project.test.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/project/test/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.project.test.service.dto;
